266342259da615ebda1c95d841f960083e9a0e22
Regina Shakirova <regina.shak@yandex.ru>
2021-02-03 01:05:35 +0300

    problem with 2 last tests

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -7,59 +7,64 @@ public class StatsService {
             sum += value;
         }
         return sum;
-}
+    }
+
     public long average(long[] values) {
         long sum = 0; // начинаем с нуля
         long average = 0;
         for (long value : values) {
             sum += value;
-            average = sum  / 12;
+            average = sum / 12;
         }
         return average;
-}
-        public long findMax (long[] values){
-            long currentMax = values[0];
-            for (long value : values) {
-                if (currentMax < value) {
-                    currentMax = value;
-                }
-            }
-            return currentMax;
-        }
-
-
-        public long findMin ( long[] values){
-            long currentMin = values[0];
-            for (long value : values) {
-                if (currentMin > value) {
-                    currentMin = value;
-                }
-            }
-            return currentMin;
-        }
-        public long calculateBelowAverage ( long[] values){
-            long average = 0;
-            for (long value : values) {
-                average = (average + value) / 12;
-                long belowAverage = values[0];
-                if (belowAverage < average) {
-                    average += 1;
-                }
-            }
-            return average;
-        }
-        public long calculateOverAverage ( long[] values){
-            long average = 0;
-            for (long value : values) {
-                average = (average + value) / 12;
-                long overAverage = values[0];
-                if (overAverage > average) {
-                    average += 1;
-                }
-            }
-            return average;
-        }
     }
+
+    public long findMax(long[] values) {
+        long currentMax = values[0];
+        for (long value : values) {
+            if (currentMax < value) {
+                currentMax = value;
+            }
+        }
+        return currentMax;
+    }
+
+
+    public long findMin(long[] values) {
+        long currentMin = values[0];
+        for (long value : values) {
+            if (currentMin > value) {
+                currentMin = value;
+            }
+        }
+        return currentMin;
+    }
+
+    public long findBelowAverage(long[] values) {
+        long sum = 0;
+        long belowAverage = 0;
+        for (long value : values) {
+            sum += value;
+            long average = sum / 12;
+            if (value < average) {
+                belowAverage += 1;
+            }
+        }
+        return belowAverage;
+    }
+
+    public long calculateOverAverage(long[] values) {
+        long average = 0;
+        for (long value : values) {
+            average = (average + value) / 12;
+            long overAverage = values[0];
+            if (overAverage > average) {
+                average += 1;
+            }
+        }
+        return average;
+    }
+}
 
 
 
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -30,4 +30,38 @@ class StatsServiceTest {
 
     }
 
+    @Test
+    void findMax() {
+        StatsService service = new StatsService();
+
+        long[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 20;
+        long actual = service.findMax(values);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMin() {
+        StatsService service = new StatsService();
+
+        long[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 7;
+        long actual = service.findMin(values);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findBelowAverage() {
+        StatsService service = new StatsService();
+
+        long[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.findBelowAverage(values);
+
+        assertEquals(expected, actual);
+
+    }
 }
